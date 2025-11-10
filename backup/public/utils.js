138aa98103328517446584905dd9343fe3002fbb
zzo3 <sso6193@gmail.com>
2025-11-11 01:32:05 +0800

    重構專案結構以提升維護性與部署安全性

--- backup/public/utils.js
+++
@@ -1,8 +0,0 @@
-const userPoints = {};
-
-function adjustPoints(user, amount) {
-  userPoints[user] = (userPoints[user] || 0) + amount;
-  if (userPoints[user] < 0) userPoints[user] = 0;
-}
-
-export { userPoints, adjustPoints };

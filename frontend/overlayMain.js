3ff0bd37e5b6bb5665da17eee2edcd925b5c9ed3
zzo3 <116035465+zzo3@users.noreply.github.com>
2025-11-11 02:20:16 +0800

    Add files via upload

--- frontend/overlayMain.js
+++ frontend/overlayMain.js
@@ -1,17 +1,6 @@
-HEAD
 import './utils.js';
 import './websocketBridge.js';
 import './streamMonitor.js';
 import './announcer.js';
 
-// 可選：初始化提示
 console.log("Overlay modules loaded.");
-
-import './utils.js';
-import './websocketBridge.js';
-import './streamMonitor.js';
-import './announcer.js';
-
-// 可選：初始化提示
-console.log("Overlay modules loaded.");
-d2de1e27b12e9a468681f74abcd152048debfbca
